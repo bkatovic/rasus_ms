9fddc8b3c3c2fe9b7310e69bcb8d82dc52577ff0
bkatovic <borna.katovic@fer.hr>
2021-01-05 19:38:51 +0100

    removed excess code

--- temperature-microservice/src/main/java/temperaturemicroservice/TemperatureMicroserviceApplication.java
+++ temperature-microservice/src/main/java/temperaturemicroservice/TemperatureMicroserviceApplication.java
@@ -7,13 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -44,22 +39,6 @@ public class TemperatureMicroserviceApplication {
 		  
 		  repo.saveAll(measurements);
 		};
-	}
-	
-	
-	
-	
-	@RestController
-	class ServiceInstanceRestController {
-		
-		@Autowired
-		private DiscoveryClient discoveryClient;
-	
-		@RequestMapping("/service-instances/{applicationName}")
-		public List<ServiceInstance> serviceInstancesByApplicationName(
-				@PathVariable String applicationName) {
-			return this.discoveryClient.getInstances(applicationName);
-		}
 	}
 
 }
