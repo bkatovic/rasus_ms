2864446bba06e4aa642fc864bedbf3b94c019108
bkatovic <borna.katovic@fer.hr>
2021-01-05 13:55:05 +0100

    modified model, fixed filereader

--- humidity-microservice/src/main/java/humiditymicroservice/HumidityMicroserviceApplication.java
+++ humidity-microservice/src/main/java/humiditymicroservice/HumidityMicroserviceApplication.java
@@ -1,5 +1,6 @@
 package humiditymicroservice;
 
+import java.io.FileReader;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- temperature-microservice/src/main/java/temperaturemicroservice/model/Measurement.java
+++ temperature-microservice/src/main/java/temperaturemicroservice/model/Measurement.java
@@ -9,16 +9,11 @@ public class Measurement {
 	@Id
 	private long Id;
 	
-	private String temperature, pressure, humidity, co, no2, so2;
+	private String temperature;
 
-	public Measurement(long Id, String temperature, String pressure, String humidity, String co, String no2, String so2) {
+	public Measurement(long Id, String temperature) {
 		this.Id = Id;
 		this.temperature = temperature;
-		this.pressure = pressure;
-		this.humidity = humidity;
-		this.co = co;
-		this.no2 = no2;
-		this.so2 = so2;
 	}
 	
 	public Measurement() {
@@ -39,45 +34,4 @@ public class Measurement {
 	public void setTemperature(String temperature) {
 		this.temperature = temperature;
 	}
-
-	public String getPressure() {
-		return pressure;
-	}
-
-	public void setPressure(String pressure) {
-		this.pressure = pressure;
-	}
-
-	public String getHumidity() {
-		return humidity;
-	}
-
-	public void setHumidity(String humidity) {
-		this.humidity = humidity;
-	}
-
-	public String getCo() {
-		return co;
-	}
-
-	public void setCo(String co) {
-		this.co = co;
-	}
-
-	public String getNo2() {
-		return no2;
-	}
-
-	public void setNo2(String no2) {
-		this.no2 = no2;
-	}
-
-	public String getSo2() {
-		return so2;
-	}
-
-	public void setSo2(String so2) {
-		this.so2 = so2;
-	}
-		
 }
